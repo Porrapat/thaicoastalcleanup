d6be30e5d4779352aa7bfbddc83077055d7e3806
NEO-TJ <koravit_ton@hotmail.com>
2017-12-02 17:41:31 +0700

    event image befor use gallery

--- application/views/admin/eventImage/body_v.php
+++ application/views/admin/eventImage/body_v.php
@@ -3,54 +3,57 @@
     <div class="row">
         <div class="col-xs-12">
 
-<div class="container">
-	<div class="row">
-		<div class="col-xs-12 col-md-12 col-lg-12">
-			<div class="panel-group">
-				<div class="panel panel-primary">
-					<div class="panel-heading text-center">
-						<h3>
-							ภาพกิจกรรมโครงการ : 
-							<div><?php echo isset($dsIccCard[0]["Project_Name"]) ? $dsIccCard[0]["Project_Name"] : "" ?></div>
-						</h3>
-					</div>
-					<div class="panel-body">
-					<!-- Upload Form -->
-						<div class="form_data">
-							<form method="post" action="<?php echo site_url('eventImage/uploadImage')?>" enctype="multipart/form-data">
-								<input type="hidden" name="iccCardId" value="<?php echo $iccCardId ?>">
-								<input type="file" multiple="multiple" name="imageFile[]">
-								<input type="submit" value="Upload image" name="submit">
-							</form>
-						</div>
-					<!-- End Upload Form -->
+					<!-- Body content -->
+					<div class="container">
+						<div class="row">
+							<div class="col-xs-12 col-md-12 col-lg-12">
+								<div class="panel-group">
+									<div class="panel panel-primary">
+										<div class="panel-heading text-center">
+											<h3>
+												ภาพกิจกรรมโครงการ : 
+												<div><?php echo isset($dsIccCard[0]["Project_Name"]) ? $dsIccCard[0]["Project_Name"] : "" ?></div>
+											</h3>
+										</div>
+										<div class="panel-body">
+										<!-- Upload Form -->
+											<div class="form_data">
+												<form method="post" action="<?php echo site_url('eventImage/uploadImage')?>" enctype="multipart/form-data">
+													<input type="hidden" name="iccCardId" value="<?php echo $iccCardId ?>">
+													<input type="file" multiple="multiple" name="imageFile[]">
+													<input type="submit" value="Upload image" name="submit">
+												</form>
+											</div>
+										<!-- End Upload Form -->
 
-					<!--Uploaded Image -->
-						<?php if(isset($dsImage)) { ?>
-							<table width="50%" cellpadding="1" cellspacing="0">
-								<?php foreach($dsImage as $image) { ?>
-								<tr>
-	<!--
-									<td>
-										<img class="img" 
-										src="<?=base_url().'uploads/Event_Images/'.$image['Image_URL'] ?>">
-									</td>
-	-->
-									<td>
-										<img class="img" 
-										src="<?=base_url().'uploads/Event_Images/thumbs/'.$image['Image_URL'] ?>">
-									</td>
-								</tr>
-								<?php }?>
-							</table>
-						<?php } ?>
-					<!-- End Uploaded Image -->
+										<!--Uploaded Image -->
+											<?php if(isset($dsImage)) { ?>
+												<table width="100%" cellpadding="1" cellspacing="0">
+													<tr>
+														<?php $i=1; ?>
+														<?php foreach($dsImage as $image) { ?>
+															<td>
+																<img class="img" 
+																src="<?=base_url().'uploads/Event_Images/thumbs/'.$image['Image_URL'] ?>">
+															</td>
+															<?php if($i > 4) { ?>
+																</tr>
+																<tr>
+																<?php $i = 0; ?>
+															<?php } ?>
+															<?php $i++; ?>
+														<?php }?>
+													</tr>
+												</table>
+											<?php } ?>
+										<!-- End Uploaded Image -->
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
 					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+					<!-- End Body content -->
 
 			</div>
     </div>
