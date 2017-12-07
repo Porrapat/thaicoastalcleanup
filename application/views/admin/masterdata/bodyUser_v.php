533f4bbf722064bb653794eab513cb336918c8d4
rsiripong <siripong.rungsang@gmail.com>
2017-12-07 10:31:29 +0700

    - exit bodyuser

--- application/views/admin/masterdata/bodyUser_v.php
+++ application/views/admin/masterdata/bodyUser_v.php
@@ -49,30 +49,7 @@
 <?php echo form_open(base_url("masterdata/save"), array("id" => "formInputData")); ?>
 <input type='hidden' id='dataType' name='dataType' value=<?php echo($dataType); ?>></input>
 <input type='hidden' id='rowID' name='rowID' value=<?php echo($dsInput['masterId']); ?> />
-<input type='hidden' id='employeeId' name='FK_ID_Employee' value=<?php echo($dsInput['FK_ID_Employee']); ?> />
 <input type='hidden' id='baseUrl' value="<?php echo(base_url()); ?>"></input>
-
-<!-- First Name -->
-<div class="col-xs-12 col-md-12 col-lg-12 margin-input">
-	<div class="input-group">
-		<span class="input-group-btn">
-			<button class="btn btn-primary disabled" type="button">First Name : </button>
-		</span>
-		<input type="text" class="form-control input-require startFocus" autocomplete="off"
-			placeholder="First Name..." id="firstName" name="First_Name" value="<?php echo($dsInput['First_Name']); ?>">
-	</div>
-</div>
-
-<!-- Last Name -->
-<div class="col-xs-12 col-md-12 col-lg-12 margin-input">
-	<div class="input-group">
-		<span class="input-group-btn">
-			<button class="btn btn-primary disabled" type="button">Last Name : </button>
-		</span>
-		<input type="text" class="form-control" autocomplete="off"
-			placeholder="Last Name..." id="lastName" name="Last_Name" value="<?php echo($dsInput['Last_Name']); ?>">
-	</div>
-</div>
 
 <!-- User ID -->
 <div class="col-xs-12 col-md-12 col-lg-12 margin-input">
@@ -97,6 +74,28 @@
 	</div>
 </div>
 
+<!-- First Name -->
+<div class="col-xs-12 col-md-12 col-lg-12 margin-input">
+	<div class="input-group">
+		<span class="input-group-btn">
+			<button class="btn btn-primary disabled" type="button">First Name : </button>
+		</span>
+		<input type="text" class="form-control input-require startFocus" autocomplete="off"
+			placeholder="First Name..." id="firstName" name="First_Name" value="<?php echo($dsInput['First_Name']); ?>">
+	</div>
+</div>
+
+<!-- Last Name -->
+<div class="col-xs-12 col-md-12 col-lg-12 margin-input">
+	<div class="input-group">
+		<span class="input-group-btn">
+			<button class="btn btn-primary disabled" type="button">Last Name : </button>
+		</span>
+		<input type="text" class="form-control" autocomplete="off"
+			placeholder="Last Name..." id="lastName" name="Last_Name" value="<?php echo($dsInput['Last_Name']); ?>">
+	</div>
+</div>
+
 <!-- Emial -->
 <div class="col-xs-12 col-md-12 col-lg-12 margin-input">
 	<div class="input-group">
@@ -105,66 +104,6 @@
 		</span>
 		<input type="text" class="form-control" autocomplete="off"
 			placeholder="Email@..." id="email" name="Email" value="<?php echo($dsInput['Email']); ?>">
-	</div>
-</div>
-
-<!-- Level -->
-<div class="col-xs-12 col-md-12 col-lg-12 margin-input">
-	<div class="input-group">
-		<span class="input-group-btn">
-			<button class="btn btn-primary disabled" type="button">Level : </button>
-		</span>
-		<select class="form-control" id="level" name="Level">
-			<option value="1" <?php echo(($dsInput['Level'] == '1') ? ' selected' : ''); ?>>Admin</option>
-			<option value="2" <?php echo(($dsInput['Level'] == '2') ? ' selected' : ''); ?>>Supervisor/Engineer</option>
-			<option value="3" <?php echo(($dsInput['Level'] == '3') ? ' selected' : ''); ?>>Staff</option>
-		</select>
-	</div>
-</div>
-
-<!-- Active -->
-<div class="col-xs-12 col-md-12 col-lg-12 margin-input">
-	<div class="input-group">
-		<span class="input-group-btn">
-			<button class="btn btn-primary disabled" type="button">Active : </button>
-		</span>
-		<select class="form-control" id="active" name="Active">
-			<option value="0"<?php echo(($dsInput['Active'] == 0) ? ' selected' : ''); ?>>Terminate</option>
-			<option value="1"<?php echo(($dsInput['Active'] == 1) ? ' selected' : ''); ?>>Active</option>
-		</select>
-	</div>
-</div>
-
-<!-- Job Title -->
-<div class="col-xs-12 col-md-12 col-lg-12 margin-input">
-	<div class="input-group">
-		<span class="input-group-btn">
-			<button class="btn btn-primary disabled" type="button">Job Title : </button>
-		</span>
-		<input type="text" class="form-control" autocomplete="off"
-			placeholder="Job Title..." id="jobTitle" name="Job_Title" value="<?php echo($dsInput['Job_Title']); ?>">
-	</div>
-</div>
-
-<!-- Department -->
-<div class="col-xs-12 col-md-12 col-lg-12 margin-input">
-	<div class="input-group">
-		<span class="input-group-btn">
-			<button class="btn btn-primary disabled" type="button">Department : </button>
-		</span>
-		<input type="text" class="form-control" autocomplete="off"
-			placeholder="Department..." id="department" name="Department" value="<?php echo($dsInput['Department']); ?>">
-	</div>
-</div>
-
-<!-- Workshop -->
-<div class="col-xs-12 col-md-12 col-lg-12 margin-input">
-	<div class="input-group">
-		<span class="input-group-btn">
-			<button class="btn btn-primary disabled" type="button">Workshop : </button>
-		</span>
-		<input type="text" class="form-control" autocomplete="off"
-			placeholder="Workshop..." id="workshop" name="Workshop" value="<?php echo($dsInput['Workshop']); ?>">
 	</div>
 </div>
 
@@ -200,6 +139,55 @@
 		</span>
 		<input type="text" class="form-control" autocomplete="off"
 			placeholder="ID Card Number..." id="idCardNumber" name="ID_Card_Number" value="<?php echo($dsInput['ID_Card_Number']); ?>">
+	</div>
+</div>
+
+<!-- Level -->
+<div class="col-xs-12 col-md-12 col-lg-12 margin-input">
+	<div class="input-group">
+		<span class="input-group-btn">
+			<button class="btn btn-primary disabled" type="button">Level : </button>
+		</span>
+		<select class="form-control" id="level" name="Level">
+			<option value="1" <?php echo(($dsInput['Level'] == '1') ? ' selected' : ''); ?>>ผู้ดูแลระบบ</option>
+			<option value="2" <?php echo(($dsInput['Level'] == '2') ? ' selected' : ''); ?>>ชำนาญการ</option>
+			<option value="3" <?php echo(($dsInput['Level'] == '3') ? ' selected' : ''); ?>>ปฏิบัติการ</option>
+			<option value="4" <?php echo(($dsInput['Level'] == '3') ? ' selected' : ''); ?>>อาสาสมัคร</option>
+		</select>
+	</div>
+</div>
+
+<!-- Department -->
+<div class="col-xs-12 col-md-12 col-lg-12 margin-input">
+	<div class="input-group">
+		<span class="input-group-btn">
+			<button class="btn btn-primary disabled" type="button">Department : </button>
+		</span>
+		<select class="form-control input-require startFocus" id="department" name="FK_Department">
+			<option value="0" selected>กรุณาเลือกหน่วยงานที่สังกัด</option>
+			<?php 
+				foreach($dsDepartment as $row) {
+					$selected = (($dsInput['FK_Department'] == $row['id']) 
+								? ' selected' : '');
+					echo '<option value='.$row['id'].$selected.'>'.$row['department'].'</option>';
+				}
+			?>
+		</select>
+	</div>
+</div>
+
+<!-- Status -->
+<div class="col-xs-12 col-md-12 col-lg-12 margin-input">
+	<div class="input-group">
+		<span class="input-group-btn">
+			<button class="btn btn-primary disabled" type="button">Status : </button>
+		</span>
+		<select class="form-control" id="status" name="Status">
+			<option value="0"<?php echo(($dsInput['Status'] == 0) ? ' selected' : ''); ?>>ยังไม่เปิดใช้งาน</option>
+			<option value="1"<?php echo(($dsInput['Status'] == 1) ? ' selected' : ''); ?>>พร้อมใช้งาน</option>
+			<option value="2"<?php echo(($dsInput['Status'] == 2) ? ' selected' : ''); ?>>รอการยืนยัน</option>
+			<option value="3"<?php echo(($dsInput['Status'] == 3) ? ' selected' : ''); ?>>รหัสถูกล๊อค</option>
+		</select>
 	</div>
 </div>
 
